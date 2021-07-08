86ef9b8b0dd0e2a703ac038ebb4214dafa8a70a6
Ivan Adamchuk <adamchuk.v.1@gmail.com>
2021-07-08 09:24:37 +0300

    Add posibility to learn verbs

--- js/learn.js
+++ js/learn.js
@@ -1,11 +1,12 @@
-var unknown_words = [];
-var user_answers = [];
 var span_number = document.getElementById("quiz-number");
 var span_word = document.getElementById("quiz-word");
-var user_answer = document.getElementById("user-answer");
+var input_answer = document.getElementById("user-answer");
+var unknown_words = [];
+var user_answers = [];
 var current_word = 1;
+var current_index = 0;
 
-function LearnMyWords()
+document.addEventListener("DOMContentLoaded", function()
 {
   for(var i = 0; i < words.length; i++)
   {
@@ -16,33 +17,27 @@ function LearnMyWords()
   }
 
   span_number.innerHTML = current_word + "/" + unknown_words.length;
-  span_word.innerHTML = words[unknown_words[0]][0];
-}
-
-document.getElementById("give-answer").addEventListener("click", GiveAnswer);
-document.getElementById("user-answer").addEventListener("keyup", function(event)
-{
-  if (event.keyCode === 13)
-  {
-    event.preventDefault();
-    document.getElementById("give-answer").click();
-  }
+  span_word.innerHTML = words[unknown_words[current_index]][0];
 });
 
-function GiveAnswer()
+document.getElementById("give-answer").addEventListener("click", function()
 {
   if(user_answers.length < unknown_words.length)
   {
-    span_number.innerHTML = current_word + "/" + unknown_words.length;
-    span_word.innerHTML = words[current_word][0];
-    user_answers.push(user_answer.value);
-    user_answer.value = "";
+    
+    current_index += (current_index < unknown_words.length - 1);
+    span_word.innerHTML = words[unknown_words[current_index]][0];
+    user_answers.push(input_answer.value);
+    input_answer.value = "";
 
     current_word += (current_word < unknown_words.length);
+    
+    span_number.innerHTML = current_word + "/" + unknown_words.length;
   }
 
   var count_of_right = 0;
   var incorect = [];
+  var incorect_answers = [];
 
   if(user_answers.length == unknown_words.length)
   {
@@ -54,25 +49,29 @@ function GiveAnswer()
       }
       else
       {
-        incorect.push(i);
+        incorect.push(unknown_words[i]);
+        incorect_answers.push(user_answers[i]);
       }
     }
+
+    setCookie("result: number of rigth answers", count_of_right, 1);
+    setCookie("result: number of unknown words", unknown_words.length, 1);
 
     if(incorect.length)
     {
-      var answer = "Результат: " + count_of_right + "/" + unknown_words.length + "\n\nПомилки:\n";
-      
-      for(var i = 0; i < incorect.length; i++)
-      {
-        answer += user_answers[incorect[i]] + " (" + words[incorect[i]][0] + " - " + words[incorect[i]][1] + " - " + words[incorect[i]][2] + ")\n"; 
-      }
-
-      alert(answer);
-    }
-    else{
-      alert("Результат: " + count_of_right + "/" + unknown_words.length);
+      setCookie("result: mistakes", incorect, 1);
+      setCookie("result: mistakes answers", incorect_answers, 1);
     }
     
-    document.location.href = "learn-list.html";
+    document.location.href = "result.html";
   }
-}
+});
+
+document.getElementById("user-answer").addEventListener("keyup", function(event)
+{
+  if (event.keyCode === 13)
+  {
+    event.preventDefault();
+    document.getElementById("give-answer").click();
+  }
+});
